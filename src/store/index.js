1f8e1aa79d24d913766e104f39fdeaf90faf800b
NEWDSA <953627561@qq.com>
2020-04-04 23:57:20 +0800

    使用vuex进行状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        // 定义的共享状态
-        isTabbarshow: true
+  state: {
+    // 定义的共享状态
+    isTabbarshow: true,
+    commingList: []
+
+  },
+  mutations: { // 唯一修改状态的位置
+    HideMaizuoTabbar (state, data) {
+      console.log('HideMaizuoTabbar' + data)
+      state.isTabbarshow = data
     },
-    mutations: { // 唯一修改状态的位置
-        HideMaizuoTabbar(state, data) {
-            console.log('HideMaizuoTabbar' + data)
-            state.isTabbarshow = data
-        },
-        ShowMaizuoTabbar(state, data) {
-            state.isTabbarshow = data
-            console.log('ShowMaizuoTabbar' + data)
+    ShowMaizuoTabbar (state, data) {
+      state.isTabbarshow = data
+      console.log('ShowMaizuoTabbar' + data)
+    },
+    comingListMutation (state, data) {
+      state.commingList = data
+    }
+  },
+  actions: {
+    // 异步处理
+    getComingListAction (store) {
+      Axios({
+        url: 'https://m.maizuo.com/gateway?cityId=110100&pageNum=1&pageSize=10&type=2&k=492530',
+        headers: {
+          'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.0.4","e":"15856408991189705941043","bc":"110100"}',
+          'X-Host': 'mall.film-ticket.film.list'
         }
-    },
-    actions: {},
-    modules: {}
+      }).then(res => {
+        console.log(res.data)
+        store.commit('comingListMutation', res.data.data.films)
+      })
+    }
+  },
+  modules: {}
 })
--- src/views/Film/Comingsoon.vue
+++ src/views/Film/Comingsoon.vue
@@ -1,5 +1,36 @@
 <template>
     <div>
-        Comingsoon
+   <ul>
+        <li v-for="data in $store.state.commingList" :key="data.filmId">
+             <img :src="data.poster"/>
+             <h3>{{data.name}}</h3>
+             <p>观众评分:{{data.grade}}</p>
+             <p>主演:{{data.actors | actorfilter}}</p>
+         </li>
+        </ul>
     </div>
 </template>
+<script>
+export default {
+  mounted () {
+    if (this.$store.state.commingList.length === 0) {
+      // ajax请求
+      this.$store.dispatch('getComingListAction') // dispatch 分发、触发
+    } else {
+      console.log('使用缓存数据')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+    ul{
+      li{
+        padding: 10px;
+        overflow: hidden;
+        img{
+          float: left;
+          width: 100px;
+        }
+      }
+    }
+</style>
