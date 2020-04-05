979079832a43cbbbbcbc29d149315edd26898923
NEWDAS <953627561@qq.com>
2020-04-05 18:40:46 +0800

    mint-ui使用

--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ import { A1 as mya1 } from './module/moudleB' // 只導入某幾個接口
 
 // 引入element UI
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI) // 註冊、使用ui庫
+import 'element-ui/lib/theme-chalk/index.css' // 註冊、使用ui庫
+
+// 引入 mint-ui
+// import { Button } from 'mint-ui/lib/button'
+// Vue.component(Button.name, Button)
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 console.log(AllA, mya1)
 new Vue({
--- src/views/Center.vue
+++ src/views/Center.vue
@@ -9,30 +9,32 @@
     <el-progress :percentage="100" status="success"></el-progress>
     <el-progress :percentage="100" status="warning"></el-progress>
     <el-progress :percentage="50" status="exception"></el-progress>
+    <mt-button type="danger">danger</mt-button>
   </div>
 </template>
 <script>
 export default {
-  data() {
+  data () {
     return {
-      radio: "1"
-    };
+      radio: '1'
+    }
   },
-  methods :{
-    format(percentage) {
-        return percentage === 100 ? '满' : `${percentage}%`;
+  methods: {
+    format (percentage) {
+      return percentage === 100 ? '满' : `${percentage}%`
+    }
   },
-  beforeRouteEnter(to, from, next) {
-    console.log("ju bu pan cha");
-    const a = true;
+  beforeRouteEnter (to, from, next) {
+    console.log('ju bu pan cha')
+    const a = true
     if (a) {
-      next();
+      next()
     } else {
-      next("/login");
+      next('/login')
     }
     // 在渲染该组件的对应路由被 confirm 前调用
     // 不！能！获取组件实例 `this`
     // 因为当守卫执行前，组件实例还没被创建
   }
-};
+}
 </script>
