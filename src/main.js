dda897dbd791d9b94c050018c4f5b12a05588fb5
NEWDAS <953627561@qq.com>
2020-04-05 15:06:23 +0800

    es6 中模塊導入以及導出

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import AllA from './module/moduleA'     //導入所有接口
-import { A1 as mya1 } from './module/moudleB'  //只導入某幾個接口
+import AllA from './module/moduleA' // 導入所有接口
+import { A1 as mya1 } from './module/moudleB' // 只導入某幾個接口
 Vue.config.productionTip = false
 
 console.log(AllA, mya1)
